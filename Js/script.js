2eddc6b3f67ba87b44b85d2dddf3f6abf4b1099b
Ivan <retouch226@gmail.com>
2021-12-12 00:14:43 +0200

    FilmSite 0.2ver

--- Js/script.js
+++ Js/script.js
@@ -1,137 +1,71 @@
-const movieDB = {
-  movies: ["Leon", "FightClub", "Titanic", "Rocky", "Dude"],
-};
-//remove start films
-function remove(movieDB) {
-  movieDB.movies.sort();
-  document.getElementById("watched-films").remove();
-}
-remove(movieDB);
-//new film list
-function filmList() {
-  const div = document.createElement("ul");
-  div.id = "watched-films";
-  const contentWatchedFilms = document.querySelector(".content__film-list");
-  contentWatchedFilms.appendChild(div);
-  for (let i = 0; i < movieDB.movies.length; i++) {
-    div.innerHTML += `<a href=""><li>${movieDB.movies[i]}</li></a>`;
-  }
-  console.log(document.body);
-}
-filmList();
-//addfilm
-function addFilm(e) {
-  let i = 0;
-  e.preventDefault();
-  const favFilm = document.querySelector("#fav-film");
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const movieDB = {
+    movies: ["Leon", "FightClub", "Titanic", "Rocky", "Dude"],
+  };
 
-  const watchedFilm = document.getElementById("enter-film").value;
-  if (movieDB.movies.length == 0) {
-    movieDB.movies.push(watchedFilm);
-  } else {
-    while (watchedFilm != movieDB.movies[i] || i < movieDB.movies.length) {
-      i++;
-      iscopy = false;
-      if (watchedFilm == movieDB.movies[i]) {
-        alert("film exist already");
-        break;
-      }
-
-      if (i == movieDB.movies.length) {
-        movieDB.movies[movieDB.movies.length] = watchedFilm;
-        if (favFilm.value == "on") {
-          console.log(watchedFilm + "is favorite film!");
-        }
-        console.log(movieDB.movies);
-        const a = document.createElement("a");
-        a.href = "#";
-        if (watchedFilm.length >= 21) {
-          a.innerHTML = `<li>${newFilmLength(watchedFilm)}</li>`;
-          const ul = document.getElementById("watched-films");
-          ul.append(a);
-          addTrash();
-          //addTrash(movieDB.movies.length - 1);
-          break;
-        } else {
-          a.innerHTML = `<li>${movieDB.movies[movieDB.movies.length - 1]}</li>`;
-          const ul = document.getElementById("watched-films");
-          ul.append(a);
-          addTrash();
-          //addTrash(movieDB.movies.length - 1);
-          break;
-        }
-      }
-    }
-  }
-}
-//checklength
-function newFilmLength(film) {
-  const filmsplit = film.split("");
-  filmsplit.splice(21, filmsplit.length - 21, "...");
-  filmsplit.join("");
-  return filmsplit;
-}
-
-function addTrash(newfilm) {
-  const ulWatchedFilms = document.querySelector("#watched-films");
-  const filmList = ulWatchedFilms.querySelectorAll("a");
-  let isbutton = false;
-  let filmListNodes;
-  filmList.forEach((item, index) => {
-    if (item.hasChildNodes) {
-      filmListNodes = item.childNodes;
-    }
-
-    for (let i = 0; i < filmListNodes.length; i++) {
-      if (filmListNodes[i].nodeName != "BUTTON") {
-        isbutton = false;
-      } else {
-        isbutton = true;
-      }
-    }
-    if (isbutton == false && newfilm == undefined) {
-      const button = document.createElement("button");
-      button.id = index;
-      button.classList = "trash";
-      item.append(button);
-    }
-  });
-  console.log(filmList);
-  console.log(document.body);
-  removeFilm();
-}
-addTrash();
-
-function removeFilm() {
-  const ulWatchedFilms = document.querySelector("#watched-films");
-  const trashButtons = ulWatchedFilms.querySelectorAll("button");
-
-  //console.log(trashButtons);
-  // for (let i = 0; i < trashButtons; i++) {
-  //   trashButtons[i].addEventListener("click", (e) => {
-  //     e.preventDefault();
-  //     trashButtons[i].parentNode.remove();
-  //   });
-  // }
-
-  trashButtons.forEach((item, index1) => {
-    item.addEventListener("click", (e) => {
-      e.preventDefault();
-      //const index = fruits.findIndex((fruit) => fruit === "blueberries");
-      const index = movieDB.movies.findIndex(
-        (film) => film === item.previousElementSibling.textContent
-      );
-      // let findFilm = (item) =>
-      //   movieDB.movies[1] == item.previousElementSibling.textContent;
-      movieDB.movies.splice(index, 1);
-
-      console.log(movieDB.movies);
-
-      item.parentNode.remove();
+  //Output FilmList
+  function filmList() {
+    const ulWatchedFilms = document.querySelector("#watched-films");
+    movieDB.movies.forEach((item, index) => {
+      const a = document.createElement("a");
+      a.id = "a" + index;
+      const aindex = a.id;
+      a.href = "#";
+      a.insertAdjacentHTML("afterbegin", `<li>${item}</li>`);
+      ulWatchedFilms.append(a);
+      addTrash(aindex, item);
     });
-  });
-}
-//removeFilm();
-//buttons
-const submitButton = document.querySelector("#submit");
-submitButton.addEventListener("click", addFilm);
+  }
+  filmList();
+
+  //Add TrashButton
+  function addTrash(aIndex, item) {
+    const ulWatchedFilms = document.querySelector("#watched-films");
+    const trashButton = document.createElement("button");
+    trashButton.classList = ".trash";
+    ///?
+    //const a = ulWatchedFilms.querySelector("#" + aIndex);
+    var lidelete;
+    /////////////////////////!!!!
+    const li = ulWatchedFilms.querySelectorAll("li");
+    li.forEach((itemLi) => {
+      if (itemLi.textContent == item) {
+        lidelete = itemLi;
+        itemLi.parentNode.append(trashButton);
+      }
+    });
+
+    //a.append(trashButton);
+    trashButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      const i = movieDB.movies.indexOf(item);
+      movieDB.movies.splice(i, 1);
+      console.log(movieDB.movies);
+      trashButton.parentNode.remove();
+    });
+  }
+
+  //AddFilm
+  function addFilm(e) {
+    e.preventDefault();
+    const newFilm = document.querySelector("#enter-film").value;
+    if (movieDB.movies.indexOf(newFilm) == -1) {
+      movieDB.movies.push(newFilm);
+      console.log(movieDB.movies);
+      /////?
+      const aindex = 1;
+      const ulWatchedFilms = document.querySelector("#watched-films");
+      const a = document.createElement("a");
+      a.href = "#";
+      a.insertAdjacentHTML("afterbegin", `<li>${newFilm}</li>`);
+      ulWatchedFilms.append(a);
+      addTrash(aindex, newFilm);
+    } else {
+      alert(`${newFilm} exist already!`);
+    }
+  }
+
+  const submitButton = document.querySelector("#submit");
+  submitButton.addEventListener("click", addFilm);
+});
