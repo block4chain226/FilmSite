1c403b6c80e72a77e2cb0955af5862269062398f
Ivan <retouch226@gmail.com>
2021-12-12 00:18:14 +0200

    FilmSite 0.2ver

--- Js/script.js
+++ Js/script.js
@@ -7,27 +7,22 @@ document.addEventListener("DOMContentLoaded", () => {
   //Output FilmList
   function filmList() {
     const ulWatchedFilms = document.querySelector("#watched-films");
-    movieDB.movies.forEach((item, index) => {
+    movieDB.movies.forEach((item) => {
       const a = document.createElement("a");
-      a.id = "a" + index;
-      const aindex = a.id;
       a.href = "#";
       a.insertAdjacentHTML("afterbegin", `<li>${item}</li>`);
       ulWatchedFilms.append(a);
-      addTrash(aindex, item);
+      addTrash(item);
     });
   }
   filmList();
 
   //Add TrashButton
-  function addTrash(aIndex, item) {
+  function addTrash(item) {
     const ulWatchedFilms = document.querySelector("#watched-films");
     const trashButton = document.createElement("button");
     trashButton.classList = ".trash";
-    ///?
-    //const a = ulWatchedFilms.querySelector("#" + aIndex);
     var lidelete;
-    /////////////////////////!!!!
     const li = ulWatchedFilms.querySelectorAll("li");
     li.forEach((itemLi) => {
       if (itemLi.textContent == item) {
@@ -35,8 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
         itemLi.parentNode.append(trashButton);
       }
     });
-
-    //a.append(trashButton);
     trashButton.addEventListener("click", (e) => {
       e.preventDefault();
       const i = movieDB.movies.indexOf(item);
@@ -53,14 +46,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (movieDB.movies.indexOf(newFilm) == -1) {
       movieDB.movies.push(newFilm);
       console.log(movieDB.movies);
-      /////?
-      const aindex = 1;
       const ulWatchedFilms = document.querySelector("#watched-films");
       const a = document.createElement("a");
       a.href = "#";
       a.insertAdjacentHTML("afterbegin", `<li>${newFilm}</li>`);
       ulWatchedFilms.append(a);
-      addTrash(aindex, newFilm);
+      addTrash(newFilm);
     } else {
       alert(`${newFilm} exist already!`);
     }
