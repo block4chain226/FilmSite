2da9e23fcc81b70ca0bd2ebde280a2760003087c
Ivan <retouch226@gmail.com>
2021-12-12 17:29:48 +0200

    FilmSite 0.3-new_film_length

--- Js/script.js
+++ Js/script.js
@@ -4,8 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     movies: ["Leon", "FightClub", "Titanic", "Rocky", "Dude"],
   };
 
+  //Sort
+  function moviesSort() {
+    movieDB.movies.sort();
+  }
+
   //Output FilmList
   function filmList() {
+    moviesSort();
     const ulWatchedFilms = document.querySelector("#watched-films");
     movieDB.movies.forEach((item) => {
       const a = document.createElement("a");
@@ -24,8 +30,10 @@ document.addEventListener("DOMContentLoaded", () => {
     trashButton.classList = ".trash";
     var lidelete;
     const li = ulWatchedFilms.querySelectorAll("li");
+    const itemPoints = item + "...";
+    console.log(itemPoints);
     li.forEach((itemLi) => {
-      if (itemLi.textContent == item) {
+      if (itemLi.textContent == item || itemLi.textContent.length > 21) {
         lidelete = itemLi;
         itemLi.parentNode.append(trashButton);
       }
@@ -43,18 +51,37 @@ document.addEventListener("DOMContentLoaded", () => {
   function addFilm(e) {
     e.preventDefault();
     const newFilm = document.querySelector("#enter-film").value;
-    if (movieDB.movies.indexOf(newFilm) == -1) {
+    if (
+      movieDB.movies.indexOf(newFilm) == -1 &&
+      newFilm != "" &&
+      newFilm.length > 1 &&
+      newFilm != "0"
+    ) {
       movieDB.movies.push(newFilm);
-      console.log(movieDB.movies);
+      moviesSort();
+      console.log(movieDB.movies, newFilm);
       const ulWatchedFilms = document.querySelector("#watched-films");
       const a = document.createElement("a");
       a.href = "#";
-      a.insertAdjacentHTML("afterbegin", `<li>${newFilm}</li>`);
+      a.insertAdjacentHTML("afterbegin", `<li>${newFilmLength(newFilm)}</li>`);
       ulWatchedFilms.append(a);
       addTrash(newFilm);
+      if (document.querySelector("#fav-film").value == "on") {
+        console.log("favorite");
+      }
     } else {
       alert(`${newFilm} exist already!`);
     }
+  }
+
+  function newFilmLength(film) {
+    if (film.length > 21) {
+      let filmList = film.split("");
+      console.log(filmList);
+      filmList.splice(21, filmList.length - 21, "...");
+      return filmList.join("");
+    }
+    return film;
   }
 
   const submitButton = document.querySelector("#submit");
